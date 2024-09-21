34917971cbe5364e66dda87a52fc88db6f8a6592
Vladislav <mindlessmuse.666@gmail.com>
2024-09-21 19:01:03 +0300

    feat, refactor
    1. Main() is divided into separate methods.
    2. Added infinite loop for solving examples.
    3. Now the user decides whether he wants to calculate the next examples or quit the program (cheskStorCalculatioin() method).

--- ExampleGenerationPracticalWork4.cpp
+++ ExampleGenerationPracticalWork4.cpp
@@ -2,41 +2,68 @@
 
 using namespace std;
 
+float _firstNumber;
+float _secondNumber;
+char _operation;
+
+void userInput() {
+	system("cls");
+
+	cout << "Введите первое число: ";
+	cin >> _firstNumber;
+
+	cout << "\nВведите второе число: ";
+	cin >> _secondNumber;
+
+	cout << "\nВведите операцию (+, -, *, / , %): ";
+	cin >> _operation;
+}
+
+void calculateExample() {
+	if (_secondNumber == 0 && (_operation == '/' || _operation == '%')) {
+		cout << "\n\x1b[91mОшибка!\x1b[0m Делить на 0 нельзя.\n";
+		return;
+	}
+
+	if (_operation == '+')
+		cout << "\n" << _firstNumber << " + " << _secondNumber << " = " << "\x1b[95m" << _firstNumber + _secondNumber << "\x1b[0m" << endl;
+	else if (_operation == '-')
+		cout << "\n" << _firstNumber << " - " << _secondNumber << " = " << "\x1b[95m" << _firstNumber - _secondNumber << "\x1b[0m" << endl;
+	else if (_operation == '*')
+		cout << "\n" << _firstNumber << " * " << _secondNumber << " = " << "\x1b[95m" << _firstNumber * _secondNumber << "\x1b[0m" << endl;
+	else if (_operation == '/')
+		cout << "\n" << _firstNumber << " / " << _secondNumber << " = " << "\x1b[95m" << _firstNumber / _secondNumber << "\x1b[0m" << endl;
+	else if (_operation == '%')
+		cout << "\n" << _firstNumber << " % " << _secondNumber << " = " << "\x1b[95m" << (int)_firstNumber % (int)_secondNumber << "\x1b[0m" << endl;
+}
+
+bool checkStopСalculation() {
+	cout << "\nВведите \x1b[95m\"+\"\x1b[0m, чтобы ввести следующий пример или \x1b[95m\"-\"\x1b[0m, если хотите завершить программу.";
+
+	char userInputChar;
+	cin >> userInputChar;
+
+	while (userInputChar == '+' || userInputChar == '-')
+	{
+		if (userInputChar == '+')
+			return true;
+		else if (userInputChar == '-')
+			return false;
+		else
+			cout << "\nВведён некоректный символ, повторите ввод.";
+	}
+
+	return true;
+}
+
 int main()
 {
 	setlocale(0, "");
 
-	float firstNumber;
-	float secondNumber;
-	char operation;
+	do {
+		userInput();
+		calculateExample();
+	} while (checkStopСalculation());
 
-	cout << "Введите первое число: ";
-	cin >> firstNumber;
-
-	cout << "\nВведите второе число: ";
-	cin >> secondNumber;
-
-	cout << "\nВведите операцию (+, -, *, / , %): ";
-	cin >> operation;
-
-	if (secondNumber == 0 && operation == '/' || operation == '%')
-	{
-		cout << "\n\x1b[91mОшибка!\x1b[0m Делить на 0 нельзя)0))";
-		int _; cin >> _;
-		return 0;
-	}
-
-	if (operation == '+')
-		cout << firstNumber << " + " << secondNumber << " = " << "\x1b[95m" << firstNumber + secondNumber << "\x1b[0m" << endl;
-	else if (operation == '-')
-		cout << firstNumber << " - " << secondNumber << " = " << "\x1b[95m" << firstNumber - secondNumber << "\x1b[0m" << endl;
-	else if (operation == '*')
-		cout << firstNumber << " * " << secondNumber << " = " << "\x1b[95m" << firstNumber * secondNumber << "\x1b[0m" << endl;
-	else if (operation == '/')
-		cout << firstNumber << " / " << secondNumber << " = " << "\x1b[95m" << firstNumber / secondNumber << "\x1b[0m" << endl;
-	else if (operation == '%')
-		cout << firstNumber << " % " << secondNumber << " = " << "\x1b[95m" << (int)firstNumber % (int)secondNumber << "\x1b[0m" << endl;
-
-	int _; cin >> _;
 	return 0;
 }
